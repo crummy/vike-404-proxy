8f4a12323d7b5fe5fdc7570de7ae5c82cfc36334
crummy <crummynz@gmail.com>
2023-07-11 06:53:40 +1200

    Repro of our 404 proxy setup

--- server/index.ts
+++ server/index.ts
@@ -1,61 +1,61 @@
+// This file does two things:
+// 1. Serves vite+ssg content
+// 2. Redirects to earth on any 404s
+// Use it for development so you can have just a single entrypoint. Run it with `node local-dev.js`
+// NOTE: there might be a better way. See https://github.com/brillout/vite-plugin-ssr/discussions/1012#discussioncomment-6401058
+
 // This file isn't processed by Vite, see https://github.com/brillout/vite-plugin-ssr/issues/562
-// Consequently:
-//  - When changing this file, you needed to manually restart your server for your changes to take effect.
-//  - To use your environment variables defined in your .env files, you need to install dotenv, see https://vite-plugin-ssr.com/env
-//  - To use your path aliases defined in your vite.config.js, you need to tell Node.js about them, see https://vite-plugin-ssr.com/path-aliases
+//  - Consequently, the server needs be manually restarted when changing this file
 
-import express from 'express'
-import compression from 'compression'
-import { renderPage } from 'vite-plugin-ssr/server'
-import { root } from './root.js'
-const isProduction = process.env.NODE_ENV === 'production'
+import express from "express";
+import { renderPage } from "vite-plugin-ssr/server";
+const httpProxy = require("http-proxy");
 
-startServer()
+const PORT = 8080;
+startServer().then(() =>
+    console.log(`Server running at http://localhost:${PORT}`)
+);
+
+const earthProxy = httpProxy.createProxyServer();
+const EARTH = "http://localhost:4568";
 
 async function startServer() {
-  const app = express()
-
-  app.use(compression())
+  const app = express();
 
   // Vite integration
-  if (isProduction) {
-    // In production, we need to serve our static assets ourselves.
-    // (In dev, Vite's middleware serves our static assets.)
-    const sirv = (await import('sirv')).default
-    app.use(sirv(`${root}/dist/client`))
-  } else {
-    // We instantiate Vite's development server and integrate its middleware to our server.
-    // ⚠️ We instantiate it only in development. (It isn't needed in production and it
-    // would unnecessarily bloat our server in production.)
-    const vite = await import('vite')
-    const viteDevMiddleware = (
+  // We instantiate Vite's development server and integrate its middleware to our server.
+  const vite = await import("vite");
+  const viteDevMiddleware = (
       await vite.createServer({
-        root,
-        server: { middlewareMode: true }
+        configFile: "./vite.config.ts",
+        server: { middlewareMode: true },
       })
-    ).middlewares
-    app.use(viteDevMiddleware)
-  }
-
-  // ...
-  // Other middlewares (e.g. some RPC middleware such as Telefunc)
-  // ...
+  ).middlewares;
+  app.use(viteDevMiddleware);
 
   // Vite-plugin-ssr middleware. It should always be our last middleware (because it's a
   // catch-all middleware superseding any middleware placed after it).
-  app.get('*', async (req, res, next) => {
+  app.all("*", async (req, res, next) => {
     const pageContextInit = {
-      urlOriginal: req.originalUrl
-    }
-    const pageContext = await renderPage(pageContextInit)
-    const { httpResponse } = pageContext
-    if (!httpResponse) return next()
-    const { body, statusCode, contentType, earlyHints } = httpResponse
-    if (res.writeEarlyHints) res.writeEarlyHints({ link: earlyHints.map((e) => e.earlyHintLink) })
-    res.status(statusCode).type(contentType).send(body)
-  })
+      urlOriginal: req.originalUrl,
+    };
+    const pageContext = await renderPage(pageContextInit);
+    const { httpResponse } = pageContext;
+    if (!httpResponse) return next();
+    const { body, statusCode, contentType, earlyHints } = httpResponse;
 
-  const port = process.env.PORT || 3000
-  app.listen(port)
-  console.log(`Server running at http://localhost:${port}`)
+    // our earth redirect. possibly this should be another middleware
+    if (statusCode === 404) {
+      earthProxy.web(req, res, { target: EARTH });
+      return;
+    }
+
+    if (res.writeEarlyHints)
+      res.writeEarlyHints({
+        link: earlyHints.map((e) => e.earlyHintLink),
+      });
+    res.status(statusCode).type(contentType).send(body);
+  });
+
+  app.listen(PORT, "0.0.0.0");
 }
